55bc0abcf4134ffd13632a474f8d9552941e2f1a
Nicolas Légat <cnle@alphanetworks.be>
2016-04-10 17:47:05 +0200

    Added decorator

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Is viewed as:
 * stdout_type
   * STDOUT_STANDARD  (Stdout doesn't change but it's not recommended to print anything in the console while the spinner is spinning)
   * STDOUT_REDIRECT (default) (Stdout is redirected to an internal list and will be printed without affecting the spinner)
-  * STDOUT DISABLE (Stdout is disabled)
+  * STDOUT_DISABLE (Stdout is disabled)
 * new_line (boolen default=True): If set to false, the latest line shown will be erased
 
 ## Warnings
--- spinloader.py
+++ spinloader.py
@@ -4,7 +4,6 @@ import os
 import sys
 from threading import Thread, RLock
 from time import sleep, time
-from io import StringIO
 
 class LoadSpinnerException(Exception):
     pass
@@ -20,6 +19,28 @@ class ListStream:
     def flush(self):
         pass
 
+
+class AbstractSpinner:
+    def get_chars(self):
+        raise NotImplementedError()
+
+    def get_iterator(self):
+        chars = self.get_chars()
+        if not isinstance(chars, str) or len(chars) <= 0:
+            raise Exception('"get_chars()" must return a non-empty string')
+        while True:
+            for c in chars:
+                yield c
+
+class BarSpinner(AbstractSpinner):
+    def get_chars(self):
+        return '|/-\\'
+
+
+class StarSpinner(AbstractSpinner):
+    def get_chars(self):
+        return '°*'
+
 class LoadSpinner:
   
     _running_lock =  RLock()
@@ -34,7 +55,8 @@ class LoadSpinner:
     STDOUT_DISABLE = 1
     STDOUT_REDIRECT = 2
 
-    def __init__(self, text='', speed=NORMAL, new_line=True, stdout_type=STDOUT_REDIRECT):
+    def __init__(self, text='', speed=NORMAL, new_line=True,
+                 stdout_type=STDOUT_REDIRECT, spinner=BarSpinner()):
         self.speed = int(speed)
         self.text = str(text)
         self.new_line = bool(new_line)
@@ -42,17 +64,29 @@ class LoadSpinner:
         self._stopped = True
         self._thread = None
         self._list_stdout = ListStream()
-        self._spinchar = self._next_cursor()
+        self.update_spinner(spinner, accept_none=False)
         self._dirty_txt = False
         self._next_txt = ''
     
-    def start(self, raise_exception=False):
-        if LoadSpinner._running is True:
-            if raise_exception:
-                raise LoadSpinnerException('Impossible to start: Already spinning')
+    def update_spinner(self, spinner, accept_none=True):
+        if spinner is None:
+            if not accept_none:
+                raise Exception('Spinner can\'t be None')
             return
-        LoadSpinner._running_lock = True
-        self._stopped = False
+        if not isinstance(spinner, AbstractSpinner):
+            raise Exception("Spinner must be an AbstractSpinner")
+        self._spinchar = spinner.get_iterator()
+        
+        
+    
+    def start(self, raise_exception=False):
+        with LoadSpinner._running_lock:
+            if LoadSpinner._running is True:
+                if raise_exception:
+                    raise LoadSpinnerException('Impossible to start: Already spinning')
+                return
+            LoadSpinner._running = True
+            self._stopped = False
         self._out = sys.__stdout__
         if self._stdout_type == self.STDOUT_DISABLE:
             sys.stdout = open(os.devnull, 'w')
@@ -62,32 +96,33 @@ class LoadSpinner:
         self._thread.start()
 
     def stop(self, raise_exception=False):
-        if LoadSpinner._running_lock is False:
-            if raise_exception:
-                raise LoadSpinnerException('No spinner running')
-            return
-        if self._stopped is True or self._thread is None:
-            if raise_exception:
-                raise LoadSpinnerException('This spinner is not currently spinning')
-            return
-        self._stopped = True
+        with LoadSpinner._running_lock:
+            if LoadSpinner._running is False:
+                if raise_exception:
+                    raise LoadSpinnerException('No spinner running')
+                return
+            if self._stopped is True or self._thread is None:
+                if raise_exception:
+                    raise LoadSpinnerException('This spinner is not currently spinning')
+                return
+            self._stopped = True
         self._thread.join()
+        LoadSpinner._running = False
         self._thread = None
         self._print_queue()
+        self._clear_loading_line()
+        self._out.write(self.text)
         if self.new_line:
-            self._out.write('\b \n')
+            self._out.write('\n')
             self._out.flush()
         else:
             self._clear_loading_line()
+        sys.stdin.flush()
         sys.stdout = sys.__stdout__
 
-    def _next_cursor(self):
-        while True:
-            for c in '|/-\\':
-                yield c
 
     def _clear_loading_line(self):
-        white_spaces = ' ' * (len(self.text) + 1)
+        white_spaces = ' ' * (len(self.text) + 5)
         self._out.write('\r{0}\r'.format(white_spaces))
         self._out.flush()
 
@@ -100,8 +135,7 @@ class LoadSpinner:
             self._check_dirty_text()
             self._print_queue()
             if time() - start_time >= refresh_frequency:
-                self._out.write('\b{0}'.format(next(self._spinchar)))
-                self._out.flush()
+                self._print_total_sentence()
                 start_time = time()
             sleep(sleep_time)
 
@@ -127,12 +161,16 @@ class LoadSpinner:
                 self._print_total_sentence()
 
     def _print_total_sentence(self):
-        self._out.write('{0}{1}'.format(self.text, next(self._spinchar)))
+        white_spaces = 4
+        end = '{0}{1}'.format(' ' * white_spaces, '\b' * (white_spaces - 1))
+        self._out.write('\r{0}{1}{2}'.format(self.text, next(self._spinchar), end))
         self._out.flush()
 
-    def update(self, new_txt):
-        self._next_txt = new_txt
-        self._dirty_txt = True
+    def update(self, new_txt=None, spinner=None):
+        if new_txt is not None:
+            self._next_txt = new_txt
+            self._dirty_txt = True
+        self.update_spinner(spinner)
 
     def __enter__(self):
         self.start(raise_exception=True)
@@ -141,12 +179,31 @@ class LoadSpinner:
     def __exit__(self, type, value, traceback):
         self.stop(raise_exception=True)
 
+def spindec(text='Loading...', speed=LoadSpinner.NORMAL, new_line=True,
+            spinner=BarSpinner(), stdout_type=LoadSpinner.STDOUT_REDIRECT):
+    def deco_wrapper(func):
+        def func_wrapper(*args, **kwargs):
+            with LoadSpinner(text, speed=speed, new_line=new_line,
+                             spinner=spinner, stdout_type=stdout_type):
+                return func(*args, **kwargs)
+        return func_wrapper
+    return deco_wrapper
+
+
+@spindec(text='Downloading...', new_line=False)
+def time_consuming_function():
+    sleep(8)
+
 if __name__ == '__main__':
-    with LoadSpinner('Generating keys...', speed=LoadSpinner.FAST) as ls:
+    with LoadSpinner('Generating keys...', speed=LoadSpinner.NORMAL, 
+                     new_line=False, spinner=StarSpinner()) as ls:
         sleep(4)
         print('Key generated successfully')
-        ls.update('Updating data...')
+        ls.update('Updating data...', BarSpinner())
         sleep(4)
         ls.update('Finishing...')
         sleep(2)
     print('Done')
+    sleep(2)
+    print('Downloading new things')
+    time_consuming_function()
